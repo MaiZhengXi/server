2be59c890c9860e020d8a1efaace16fd2a22f929
正熙 <avelezp@zx-studio.com>
2023-10-28 18:34:31 -0500

    n

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 
 app.post("/send-email", (req, res) => {
   const { contactName, contactEmail, contactPhone, contactMessage } = req.body;
-  console.log(req.body);
+  console.log("kkk" + req.body);
 
   const mailOptions = {
     from: "contacto@zx-studio.com",
